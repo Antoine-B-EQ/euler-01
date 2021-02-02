dc36d422c3b79fb28c013fde32c10ebbce25cb44
Antoine Bouesnard <283698@protonmail.ch>
2021-02-02 10:34:39 +0100

    Problem solved.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Euler1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int result = 0;
+            string reply = $"The sum of all the multiples of 3 or 5 below 1000 is {result}";
+            for (int n = 1; n < 1000; n++)
+            {
+                if (n % 3 == 0 || n % 5 == 0)
+                {
+                    result += n;
+                    Console.WriteLine(result);
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# euler-01
+# Euler's first problem.
+
+Finding the sum of all the multiples of 3 or 5 bellow 1000.
